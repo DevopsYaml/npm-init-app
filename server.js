1acfbad6fe8fd0fcd2b0f531e4d01384a17fc9d3
DevopsYaml <82764996+DevopsYaml@users.noreply.github.com>
2021-04-22 09:48:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,3 @@ app.get("/", function (req, res) {
   res.send("Hello world! Cloud Foudnry Node Js Demo");
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, function () {
-  console.log("app listening at port " + port);
-});
